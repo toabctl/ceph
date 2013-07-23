1071f57b1072d03d331508d140aaa3e23b1db45d
Dan Mick <dan.mick@inktank.com>
2013-07-22 21:55:08 -0700

    mon: "osd stat" needs a flush now that osdmap.print_summary() doesn't
    Fixes: #5719
    Signed-off-by: Dan Mick <dan.mick@inktank.com>

--- src/mon/OSDMonitor.cc
+++ src/mon/OSDMonitor.cc
@@ -1970,6 +1970,8 @@ bool OSDMonitor::preprocess_command(MMonCommand *m)
 
   if (prefix == "osd stat") {
     osdmap.print_summary(f.get(), ds);
+    if (f)
+      f->flush(ds);
     rdata.append(ds);
   }
   else if (prefix == "osd dump" ||
